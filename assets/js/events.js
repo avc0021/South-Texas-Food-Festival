7d157b39e0106700b91d02885f0c7d34ce886191
avc0021 <avc0021@gmail.com>
2022-03-11 19:17:09 -0600

    modularize front end js

---
+++ assets/js/events.js
@@ -0,0 +1,14 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+const createLoremIpsum = require("./helpers");
+
+$(document).ready( function() {
+
+if (window.location.href.indexOf("event") > -1) {
+    const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
+        title: "Title Placeholder",
+        subtitle: "",
+        description: ""
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const webpack = require('webpack')
 const path = require('path');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 
 module.exports = {
-  entry: './assets/js/script.js',
+  entry: {  
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
   output: {
     path: path.resolve(__dirname + '/dist'),
     filename: 'main.bundle.js'
@@ -12,6 +19,9 @@ module.exports = {
     $: "jquery",
     jQuery: "jquery"
   }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static', // the report to an HTML file in the dist folder
+  })
   ],
   mode: "development"
 };
